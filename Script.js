ee94f4ab50b9ba36277b1497b7e934f4a1925a40
CaveMan-tech <ofoikechi@gmail.com>
2020-03-27 22:55:28 +0100

    JS Task 1

---
+++ Script.js
@@ -0,0 +1,10 @@
+let name = "Okoro Ikechi";
+var courses = ["Design", "HTML", "CSS", "JavaScript"];
+console.log("Name: " + name);
+console.log("Courses: " + courses);
+var x;
+for (let x=1; x<=200; x++){
+if (x%2===0){
+console.log(x);
+}
+}
